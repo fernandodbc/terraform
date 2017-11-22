f8da7153a2c91b9bd25cb0b75579d683072656db
Your Name <you@example.com>
2017-11-22 15:54:30 +0100

    Adding subnet

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -1,3 +1,16 @@
 resource "aws_vpc" "main" {
   cidr_block = "172.23.0.0/16"
+
+  tags {
+    Name = "Fernando"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "172.23.1.0/24"
+
+  tags {
+    Name = "Fernando"
+  }
 }
