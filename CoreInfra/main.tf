4abb73ae09867805f81c3b8cbc45f80ec0fbeb2e
Your Name <you@example.com>
2017-11-22 16:10:38 +0100

    Adding route

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -15,10 +15,24 @@ resource "aws_subnet" "main" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "main" {
   vpc_id = "${aws_vpc.main.id}"
 
   tags {
     Name = "Fernando"
   }
 }
+
+resource "aws_route_table" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+
+  tags {
+    Name = "Fernando"
+  }
+}
+
