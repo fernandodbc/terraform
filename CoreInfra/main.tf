ff22377f88e1d33f0977d207215b097a9bfdf74b
Your Name <you@example.com>
2017-11-22 16:16:27 +0100

    adding route association

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -36,3 +36,7 @@ resource "aws_route_table" "main" {
   }
 }
 
+resource "aws_route_table_association" "main" {
+  subnet_id      = "${aws_subnet.main.id}"
+  route_table_id = "${aws_route_table.main.id}"
+}
